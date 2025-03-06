2c1e6ead42f5460d6269c994b73532fff552f9d1
hyejin <yhjyhw1004@naver.com>
2025-03-06 10:18:28 +0900

    "first commit"

---
+++ 0305/평균값 구하기.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for tc in range(1, T + 1):
+    arr = list(map(int, input().split()))
+
+    cnt = 0
+
+    for i in range(10):
+        cnt += arr[i] / 10
+
+        # / int // float 구분하기
+
+    print(f"#{tc} {round(cnt)}")
---
+++ 0305/홀수만 더하기.py
@@ -0,0 +1,18 @@
+T = int(input())
+
+for tc in range(1, T + 1):
+    N = list(map(int, input().split()))
+
+    cnt = 0
+
+    # 나머지가 1이면 출력 후 더하기
+    for i in range(10):
+        if N[i] % 2 == 1:
+            cnt += N[i]
+
+    print(f"#{tc} {cnt}")
+
+    # 변수 만들기
+    # 반복문 돌려서 조건에 맞는 거 찾아서 변수에 더해주기
+    # 반복문 끝나면 변수에 다 더해져 있음
+    # 변수 출력
